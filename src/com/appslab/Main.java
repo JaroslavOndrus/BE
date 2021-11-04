7abfc2a842c842ce85d422ec90d00f3564546de0
Jaroslav Ondruš <jaroslavondrus04@gmail.com>
2021-11-04 08:08:20 +0100

    add student address

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -4,25 +4,46 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Student student1 = new Student("John", 12345);
-        System.out.println(student1.getName() + ": " + student1.getStudentNumber());
+        Student student1 = new Student("Sam", 2, 123456, "Legionarska 1, 01001 Zilina");
+        Student student2 = new Student("John", 5, 456789, "Legionarska 2, 01001 Zilina");
+
+        System.out.println(student1.getStudentNumber() + ", " + student1.getPhoneNumber() + ", " + student1.getStudentAddress());
+        System.out.println(student2.getStudentNumber() + ", " + student2.getPhoneNumber() + ", " + student2.getStudentAddress());
     }
 }
 
 class Student{
-    private String Name;
-    private int StudentNumber;
 
-    public Student(String name, int studentNumber){
+    private String Name;
+    private int PhoneNumber;
+    private int StudentNumber;
+    private String StudentAddress;
+
+    public Student(String name, int studentNumber, int phoneNumber, String studentAddress){
         Name = name;
         StudentNumber = studentNumber;
+        PhoneNumber = phoneNumber;
+        StudentAddress = studentAddress;
+
     }
 
     public String getName() {
+
         return Name;
     }
 
     public int getStudentNumber() {
+
         return StudentNumber;
+    }
+
+    public int getPhoneNumber() {
+
+        return PhoneNumber;
+    }
+
+    public String getStudentAddress() {
+
+        return StudentAddress;
     }
 }
